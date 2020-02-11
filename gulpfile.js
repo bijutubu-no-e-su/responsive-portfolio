f20ceccc372cde6af9bdd2678ace904f847a7526
kaoru <kaoru@birman-mac.local>
2020-02-11 15:52:28 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+
+gulp.task('sass', () => {
+    const sass = require('gulp-sass')
+    return gulp
+            .src('./src/scss/*.scss')
+            .pipe(sass())
+            .pipe(gulp.dest('./dist/css'))
+})
+gulp.task('babel', () => {
+    const babel = require ('gulp-babel')
+    return gulp
+        .src('./src/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('./dist'))
+})
+
+gulp.task('serve', done => {
+    browserSync.init({
+        server: {
+            baseDir: './dist',
+            index: 'index.html',
+        },
+    })
+    done()
+})
+gulp.task('watch', () => {
+    const browserReload = done => {
+        browserSync.reload()
+        done()
+    }
+    gulp.watch('./dist/**/*', browserReload)
+    gulp.watch('./src/scss/*.scss', gulp.series('sass'))
+    gulp.watch('./src/js/*.js', gulp.series('babel'))
+})
+
+gulp.task('default', gulp.series('serve', 'watch'))
+
+
